3ff28fa247938d1dc96e7b70cce131067f02ca25
somayeh-najafi <59369443+somayeh-najafi@users.noreply.github.com>
2023-01-30 17:05:02 -0600

    Update install-script.sh

--- setup/vm-install-script/install-script.sh
+++ setup/vm-install-script/install-script.sh
@@ -71,7 +71,6 @@ mvn -v
 echo ".........----------------#################._.-.-JENKINS-.-._.#################----------------........."
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt upgrade
 sudo apt update
 sudo apt install -y jenkins
 systemctl daemon-reload
