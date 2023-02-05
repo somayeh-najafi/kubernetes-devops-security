a3e7ba91991df1ca311b75d89ec264c581aaa92b
somayeh-najafi <59369443+somayeh-najafi@users.noreply.github.com>
2023-02-05 17:37:05 -0600

    solving pit

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -43,12 +43,14 @@ public class NumericApplicationTests {
     @Test
     public void greaterThanFiftyMessage() throws Exception {
         this.mockMvc.perform(get("/compare/51")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string("Greater than 50"));
+                .andExpect(content().string("Greater than 50")).andExpect(content().string("Greater than 50"));
+
     }
     
     @Test
     public void welcomeMessage() throws Exception {
-         this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk());
+         this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk()).andExpect(content().string("Kubernetes DevSecOps"));
+
     }
     
 
