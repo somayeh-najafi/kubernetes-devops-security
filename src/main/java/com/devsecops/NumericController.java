2328b028f95e510a4691d4fb10dfc93d47b5a3d8
somayeh-najafi <59369443+somayeh-najafi@users.noreply.github.com>
2023-02-05 17:49:16 -0600

    removing empty lines

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -1,8 +1,6 @@
 package com.devsecops;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
