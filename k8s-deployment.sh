763a9931d68ab9afb389328b6b6a16631fa0329f
somayeh-najafi <59369443+somayeh-najafi@users.noreply.github.com>
2023-02-11 16:39:11 -0600

    now

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -5,12 +5,13 @@ kubectl -n  default get deployment ${deploymentName} > /dev/null
 
 if [[ $? -ne 0 ]]; then
    echo "deployment ${deploymentName} doesn't exist"
-   kubectl-n default apply -f k8s_deployment_service.yaml
+   kubectl -n default apply -f k8s_deployment_service.yaml
 
 else 
     echo "deployment ${deploymentName} exists"
     echo "image name - ${imageName}"
-    kubectl -n default set image deployment ${deploymentName} ${containerName}=${imageName} --record=true
+    #kubectl -n default set image deployment ${deploymentName} ${containerName}=${imageName} --record=true
+    kubectl -n default apply -f k8s_deployment_service.yaml
 
 fi
 
