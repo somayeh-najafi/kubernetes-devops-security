179686e3ef7b11cedcfe1450c961dbd9b5f528aa
somayeh-najafi <59369443+somayeh-najafi@users.noreply.github.com>
2023-02-11 17:23:49 -0600

    final

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -5,7 +5,7 @@ kubectl -n  default get deployment ${deploymentName} > /dev/null
 
 if [[ $? -ne 0 ]]; then
    echo "deployment ${deploymentName} doesn't exist"
-   kubectl -n default apply -f k8s_deployment_service.yaml
+   envsubst < k8s_deployment_service.yaml | kubectl -n default apply -f -
 
 else 
     echo "deployment ${deploymentName} exists"
