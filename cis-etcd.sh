c245c0c8970e6c8a47b71adf00681ea1fa254810
somayeh-najafi <59369443+somayeh-najafi@users.noreply.github.com>
2023-02-17 20:58:57 -0600

    adding exit

--- cis-etcd.sh
+++ cis-etcd.sh
@@ -5,6 +5,7 @@ total_fail=$(kube-bench run --targets etcd --version 1.15 --check 2.2 --json | j
 if [[ $total_fail -ne 0 ]];
     then
         echo "CIS Benchmark Failed Etcd while testing for 2.2"
+        exit 1;
 else
         echo "CIS Benchmark Passed Etcd while testing for 2.2"
 fi;
--- cis-kubelet.sh
+++ cis-kubelet.sh
@@ -5,6 +5,8 @@ total_fail=$(kube-bench run --targets node --version 1.15 --check 4.2.1,4.2.2 --
 if [[ $total_fail -ne 0 ]];
     then
         echo "CIS Benchmark Failed Kubelet while testing for 4.2.1 , 4.2.2"
+        exit 1;
+
 else
         echo "CIS Benchmark Passed Kubelet while testing for 4.2.1 , 4.2.2"
 fi;
