56697e35d51d3d7e76291f86fcf8bfbf08a7c2d4
somayeh-najafi <59369443+somayeh-najafi@users.noreply.github.com>
2023-02-17 21:09:19 -0600

    up

--- cis-etcd.sh
+++ cis-etcd.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 total_fail=$(kube-bench run --targets etcd --version 1.15 --check 2.2 --json | jq .total_fail)
-
+echo $total_fail
 if [[ "$total_fail" -ne 0 ]];
     then
         echo "CIS Benchmark Failed Etcd while testing for 2.2"
