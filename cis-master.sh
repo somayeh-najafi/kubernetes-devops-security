e7db34d84f3730949dfd9823d895309eaa037881
somayeh-najafi <59369443+somayeh-najafi@users.noreply.github.com>
2023-02-18 15:44:45 -0600

    update master CIS

--- cis-master.sh
+++ cis-master.sh
@@ -1,13 +1,12 @@
 #! /bin/bash
 
-total_fail=$(kube-bench run --targets master --version 1.15 --check 4.2.1,4.2.2 --json | jq .total_fail)
-echo $total_fail
+total_fail=$(kube-bench run --targets master --version 1.15 --check 1.2.21,1.2.23,1.2.24 --json | jq .total_fail)
 
 if [[ "$total_fail" -ne 0 ]];
     then
-        echo "CIS Benchmark Failed Master while testing for 4.2.1 , 4.2.2"
+        echo "CIS Benchmark Failed Kubelet while testing for 1.2.21 , 1.2.23 , 1.2.24"
         exit 1;
 
 else
-        echo "CIS Benchmark Passed Master while testing for 4.2.1 , 4.2.2"
+        echo "CIS Benchmark Passed Kubelet while testing for 1.2.21 , 1.2.23 , 1.2.24"
 fi;
