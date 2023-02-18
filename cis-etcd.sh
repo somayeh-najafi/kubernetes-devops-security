1107bda4fe2db96a09bd8ee74dbaca6c1d5cb5ab
somayeh-najafi <59369443+somayeh-najafi@users.noreply.github.com>
2023-02-17 21:04:33 -0600

    exitcode

--- cis-etcd.sh
+++ cis-etcd.sh
@@ -2,7 +2,7 @@
 
 total_fail=$(kube-bench run --targets etcd --version 1.15 --check 2.2 --json | jq .total_fail)
 
-if [[ $total_fail -ne 0 ]];
+if [[ "$total_fail" -ne 0 ]];
     then
         echo "CIS Benchmark Failed Etcd while testing for 2.2"
         exit 1;
